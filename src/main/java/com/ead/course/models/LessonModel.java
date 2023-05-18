087afec325d29385edda9d613a31ce190e659ed7
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-05-18 18:00:44 -0300

    Criando entidade LessonModel

---
+++ src/main/java/com/ead/course/models/LessonModel.java
@@ -0,0 +1,35 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_LESSONS")
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class LessonModel implements Serializable {
+    private static final long SERIAL_VERSION_UID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID lessonId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    @Column(nullable = false)
+    private String videoUrl;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy HH:mm:ss")
+    private LocalDateTime creationDate;
+}
