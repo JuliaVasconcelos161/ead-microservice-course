cd92758eb77b75ca93279564704cd5fedd6328a0
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-05-24 21:35:39 -0300

    Adicionando relações entre entidades

--- src/main/java/com/ead/course/models/LessonModel.java
+++ src/main/java/com/ead/course/models/LessonModel.java
@@ -2,6 +2,7 @@ package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -32,4 +33,8 @@ public class LessonModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy HH:mm:ss")
     private LocalDateTime creationDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private ModuleModel module;
 }
