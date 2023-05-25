f8db4049dad130a52fc2634a4fec02bd6b349acf
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-05-25 17:28:50 -0300

    Criando métodos delete para ModuleModel e CourseModel

--- src/main/java/com/ead/course/service/CourseService.java
+++ src/main/java/com/ead/course/service/CourseService.java
@@ -1,4 +1,7 @@
 package com.ead.course.service;
 
+import com.ead.course.models.CourseModel;
+
 public interface CourseService {
+    void delete(CourseModel courseModel);
 }
--- src/main/java/com/ead/course/service/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/service/impl/CourseServiceImpl.java
@@ -1,12 +1,41 @@
 package com.ead.course.service.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repository.CourseRepository;
+import com.ead.course.repository.LessonRepository;
+import com.ead.course.repository.ModuleRepository;
 import com.ead.course.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
     @Autowired
     private CourseRepository repository;
+
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @Autowired
+    LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if(!moduleModelList.isEmpty()) {
+            for(ModuleModel module : moduleModelList) {
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if(!lessonModelList.isEmpty())
+                    lessonRepository.deleteAll(lessonModelList);
+            }
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        repository.delete(courseModel);
+    }
 }
