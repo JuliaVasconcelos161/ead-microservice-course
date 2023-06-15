b733815bc9acf707d5292277ccac5888aba474a3
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-06-15 20:06:47 -0300

    Adaptando getAll de Modules com specification e consulta por criteria

--- src/main/java/com/ead/course/repository/ModuleRepository.java
+++ src/main/java/com/ead/course/repository/ModuleRepository.java
@@ -2,6 +2,7 @@ package com.ead.course.repository;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 @Repository
-public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+public interface ModuleRepository extends JpaRepository<ModuleModel, UUID>, JpaSpecificationExecutor<ModuleModel> {
     @Query(value = " select * from tb_modules where course_course_id = :courseId ", nativeQuery = true)
     List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
 
--- src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -1,11 +1,18 @@
 package com.ead.course.specifications;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+import java.util.UUID;
 
 public class SpecificationTemplate {
 
@@ -14,6 +21,22 @@ public class SpecificationTemplate {
             @Spec(path = "courseStatus", spec = Equal.class),
             @Spec(path = "name", spec = Like.class)
     })
-
     public interface CourseSpec extends Specification<CourseModel> {}
+
+
+    @Spec(path = "title", spec = Like.class)
+    public interface ModuleSpec extends Specification<ModuleModel> {}
+
+    @Spec(path = "title", spec = Like.class)
+    public interface LessonSpec extends Specification<LessonModel> {}
+
+    public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+            query.distinct(true);
+            Root<ModuleModel> module = root;
+            Root<CourseModel> course = query.from(CourseModel.class);
+            Expression<Collection<ModuleModel>> courseModules = course.get("modules");
+            return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, courseModules));
+        };
+    }
 }
