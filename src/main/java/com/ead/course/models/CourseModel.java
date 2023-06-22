f3561961698d0c2f7a07d2250de90623244c075a
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-06-22 09:18:34 -0300

    Criando CourseUserModel, CourseUserRepository e CourseUserService para relacionar microservices

--- src/main/java/com/ead/course/models/CourseModel.java
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -62,5 +62,7 @@ public class CourseModel implements Serializable {
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Set<ModuleModel> modules;
 
-
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private Set<CourseUserModel> courseUsers;
 }
