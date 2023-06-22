30cdb1864ce6ba9f230a47b81c618167d8780356
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-06-22 11:55:13 -0300

    Finalizando alterações do CourseController para que seja possível filtrar cursos por usuário

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -84,7 +84,10 @@ public class CourseController {
                                                            @PageableDefault(page = 0, size = 10, sort = "courseId",
                                                                    direction = Sort.Direction.ASC) Pageable pageable,
                                                            @RequestParam(required = false) UUID userId) {
-
+        if(userId != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(service.findAll(SpecificationTemplate.courseUserId(userId),
+                    pageable));
+        }
         return ResponseEntity.status(HttpStatus.OK).body(service.findAll(spec, pageable));
     }
 
