128f377c03e51657b7c82d887da53741c9c6949a
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-06-26 22:27:07 -0300

    Criando UserDto para iniciar o desenvolvimento do método getAllUsersByCourse

---
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -0,0 +1,28 @@
+package com.ead.course.controllers;
+
+import com.ead.course.models.dtos.UserDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class CourseUserController {
+    @GetMapping("/courses/{courseId}/users")
+    public ResponseEntity<Page<UserDto>> getAllUsersByCourse(
+            @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+            @PathVariable(value = "courseId") UUID courseId) {
+        return ResponseEntity.status(HttpStatus.OK).body();
+    }
+}
