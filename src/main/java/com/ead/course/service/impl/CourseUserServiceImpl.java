9d5cab6ae69da542244dbd59eb4f06d3ca256217
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-07-24 00:17:05 -0300

    feat: adicionando método que deleta courseUser

--- src/main/java/com/ead/course/service/impl/CourseUserServiceImpl.java
+++ src/main/java/com/ead/course/service/impl/CourseUserServiceImpl.java
@@ -39,4 +39,14 @@ public class CourseUserServiceImpl implements CourseUserService {
         authuserClient.postSubscriptionUserInCourse(courseUserModel.getCourse().getCourseId(), courseUserModel.getUserId());
         return courseUserModel;
     }
+
+    @Override
+    public boolean existsByUserId(UUID userId) {
+        return repository.existsByUserId(userId);
+    }
+    @Transactional
+    @Override
+    public void deleteCourseUserByUser(UUID userId) {
+        repository.deleteAllByUserId(userId);
+    }
 }
