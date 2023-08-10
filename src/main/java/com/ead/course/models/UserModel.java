8a10aaa2ab1460ee9f7d1beb136f78c836689636
Júlia Vasconcelos <juliavasconceloscrib@gmail.com>
2023-08-10 15:31:23 -0300

    refactor: preparando microservice course para se comunicar de forma assincrona

--- src/main/java/com/ead/course/models/UserModel.java
+++ src/main/java/com/ead/course/models/UserModel.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -14,18 +16,12 @@ import java.util.UUID;
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "TB_COURSES_USERS")
-public class CourseUserModel implements Serializable {
+@Table(name = "TB_USERS")
+public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private CourseModel course;
-
-    @Column(nullable = false)
     private UUID userId;
+
 
 }
--- src/main/java/com/ead/course/validation/CourseValidator.java
+++ src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,18 +1,10 @@
 package com.ead.course.validation;
 
-import com.ead.course.client.AuthuserClient;
-import com.ead.course.enums.UserType;
 import com.ead.course.models.dtos.CourseDto;
-import com.ead.course.models.dtos.UserDto;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-import org.springframework.web.client.HttpStatusCodeException;
-
-import java.util.UUID;
 
 
 @Component
@@ -21,11 +13,9 @@ public class CourseValidator implements Validator {
     @Qualifier("defaultValidator")
     private final Validator validator;
 
-    private final AuthuserClient authuserClient;
 
-    public CourseValidator(Validator defaultValidator, AuthuserClient authuserClient) {
+    public CourseValidator(Validator defaultValidator) {
         this.validator = defaultValidator;
-        this.authuserClient = authuserClient;
     }
 
     @Override
@@ -37,21 +27,21 @@ public class CourseValidator implements Validator {
     public void validate(Object o, Errors errors) {
         CourseDto courseDto = (CourseDto) o;
         validator.validate(courseDto, errors);
-        if(!errors.hasErrors())
-            validateUserInstructor(courseDto.getUserInstructor(), errors);
+//        if(!errors.hasErrors())
+//            validateUserInstructor(courseDto.getUserInstructor(), errors);
 
     }
 
-    private void validateUserInstructor(UUID userInstructor, Errors errors) {
-        ResponseEntity<UserDto> responseUserInstructor;
-        try {
-            responseUserInstructor = authuserClient.getOneUserById(userInstructor);
-            if(responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT))
-                errors.rejectValue("userInstructor","UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
-        }catch(HttpStatusCodeException e) {
-            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND))
-                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found");
-
-        }
-    }
+//    private void validateUserInstructor(UUID userInstructor, Errors errors) {
+//        ResponseEntity<UserDto> responseUserInstructor;
+//        try {
+//            responseUserInstructor = authuserClient.getOneUserById(userInstructor);
+//            if(responseUserInstructor.getBody().getUserType().equals(UserType.STUDENT))
+//                errors.rejectValue("userInstructor","UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+//        }catch(HttpStatusCodeException e) {
+//            if(e.getStatusCode().equals(HttpStatus.NOT_FOUND))
+//                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found");
+//
+//        }
+//    }
 }
